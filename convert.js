dc9e3fab26fa475b365b3ff4463318d182301567
jojo552 <39025926+jojo552@users.noreply.github.com>
2026-03-22 16:13:06 +0800

    fix: ship Netherlands flag icon locally

--- convert.js
+++ convert.js
@@ -424,7 +424,7 @@ const countriesMeta = {
     荷兰: {
         weight: 65,
         pattern: "荷兰|荷|NL|nl|Netherlands|netherlands|阿姆斯特丹|Amsterdam|amsterdam|🇳🇱",
-        icon: "https://gcore.jsdelivr.net/gh/twitter/twemoji@master/assets/72x72/1f1f3-1f1f1.png",
+        icon: "https://gcore.jsdelivr.net/gh/jojo552/override-rules@main/icons/Netherlands.png",
     },
     澳大利亚: {
         pattern: "澳洲|澳大利亚|AU|Australia|🇦🇺",
